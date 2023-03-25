aa4498df152e28b64af471ab1e347fb4409f924f
Dyllan <dphamada92@gmail.com>
2023-03-24 18:14:40 -0700

    got delete to work

--- server.js
+++ server.js
@@ -69,8 +69,35 @@ app.post("/api/notes", (req, res) => {
 
 
 //whenever you create new id 
-app.delete("/api/notes/:id", function (req, res) {
-  console.log("req params", req.params.id);
-  notesArray = notesArray.filter(note => note.id !== req.params.id);
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    let notes = JSON.parse(data);
+
+    notes = notes.filter((note) => note.id !== req.params.id);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Failed to delete note" });
+      }
+      console.log(
+        `Note with ID ${req.params.id} has been deleted from JSON file`
+      );
+
+      const response = {
+        status: "success",
+        message: `Note with ID ${req.params.id} has been deleted`,
+      };
+      console.log(response);
+      res.status(200).json(response);
+    });
+  });
 });
+  
+  
+
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
